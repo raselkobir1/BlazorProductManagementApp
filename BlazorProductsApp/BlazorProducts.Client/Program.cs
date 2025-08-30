43e82f8310e07625e35b9933cd18211211651ea6
Rasel Kabir <md.rasel@technonext.com>
2025-08-31 00:26:38 +0600

    ++ feat: Login service with face data

--- BlazorProductsApp/BlazorProducts.Client/Program.cs
+++ BlazorProductsApp/BlazorProducts.Client/Program.cs
@@ -1,10 +1,14 @@
 using BlazorProducts.Client;
+using BlazorProducts.Client.Services;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+
+// Register fake AuthService as Singleton
+builder.Services.AddSingleton<AuthService>();
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
---
+++ BlazorProductsApp/BlazorProducts.Client/Services/AuthService.cs
@@ -0,0 +1,19 @@
+﻿namespace BlazorProducts.Client.Services
+{
+    public class AuthService
+    {
+        private bool _isLoggedIn = false;
+
+        public bool IsLoggedIn => _isLoggedIn;
+
+        public void Login()
+        {
+            _isLoggedIn = true;
+        }
+
+        public void Logout()
+        {
+            _isLoggedIn = false;
+        }
+    }
+}
