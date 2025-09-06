f9adfee1434c61239addc14e133e8099aa9a86d5
Rasel Kabir <md.rasel@technonext.com>
2025-09-06 13:55:42 +0600

    ++ added JS IJSRuntime for call JS function

--- BlazorProductsApp/BlazorProducts.Client/Pages/ProductTable.razor.cs
+++ BlazorProductsApp/BlazorProducts.Client/Pages/ProductTable.razor.cs
@@ -1,10 +1,13 @@
 ﻿using Entities.Models;
 using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
 
 namespace BlazorProducts.Client.Pages
 {
     public partial class ProductTable
     {
+        [Inject]
+        public IJSRuntime Js { get; set; }
         [Parameter]
         public List<Product> Products { get; set; } = new();
         [Parameter]
@@ -16,6 +19,16 @@ namespace BlazorProducts.Client.Pages
         {
             var url = Path.Combine("/updateProduct/", id.ToString());
             NavigationManager.NavigateTo(url);
+        }
+        private async Task Delete(Guid id)
+        {
+            var product = Products.FirstOrDefault(p => p.Id.Equals(id));
+
+            var confirmed = await Js.InvokeAsync<bool>("confirm", $"Are you sure you want to delete {product.Name} product?");
+            if (confirmed)
+            {
+                await OnDeleted.InvokeAsync(id);
+            }
         }
     }
 }
